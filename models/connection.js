cb78db87b67032ba0d991f7e7c712a53ee2759b5
nabbott98 <nicholasabbott98@gmail.com>
2022-10-17 20:14:54 -0400

    Connection and middleware readied for deploy

--- models/connection.js
+++ models/connection.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 
 // connect to the database
-mongoose.connect(process.env.DATABASE_URL, {
+mongoose.connect(process.env.DEPLOYED_URL, {
 	useUnifiedTopology: true,
 	useNewUrlParser: true,
 })
